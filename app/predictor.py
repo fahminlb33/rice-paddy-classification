d4e99b7a5e9f2206f84ab4889754b72dfadf5465
fahminlb33 <fahminlb33@gmail.com>
2022-04-10 08:17:02 +0700

    Add image size limit

--- app/predictor.py
+++ app/predictor.py
@@ -5,6 +5,7 @@ import joblib
 import numpy as np
 import matplotlib.pyplot as plt
 
+from skimage.io import imread, imsave
 from skimage.transform import resize
 
 import tensorflow as tf
@@ -12,63 +13,96 @@ from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.preprocessing.image import load_img, array_to_img, img_to_array
 
 class PredictorModel:
+    MAX_HEIGHT: int = 800
     tf_model: Model = None
     class_names: List[str] = None
     initialized: bool = False
     IMG_SIZE: Tuple[int, int] = (160, 160)
 
     def load_model(self, model_root: str, tf_name: str, class_name: str) -> None:
+        # create model path using absolute path
         model_path = os.path.abspath(os.path.join(model_root, tf_name))
         name_path = os.path.abspath(os.path.join(model_root, class_name))
 
+        # load the model
         self.tf_model = load_model(model_path)
         self.class_names = joblib.load(name_path)
         self.initialized = True
 
-    def gradcam_heatmap(self, img_array, last_conv_layer_name="Conv_1", pred_index=None):
+    def constrain_image_size(self, img_path: str) -> str:
+        # load image
+        img = imread(img_path)
+
+        # check if image is too large
+        if img.shape[0] <= self.MAX_HEIGHT:
+            return img_path
+
+        # calculate aspect ratio and new image dimensions
+        aspect_ratio = img.shape[1] / img.shape[0]
+        dim = (self.MAX_HEIGHT, int(aspect_ratio * self.MAX_HEIGHT))
+
+        # resize image
+        img = resize(img, dim)
+
+        # save image
+        new_name = os.path.splitext(img_path)[0] + "_resized.jpg"
+        new_path = os.path.join(os.path.dirname(img_path), new_name)        
+        imsave(new_path, img)
+
+        return new_path
+
+    def create_gradcam_matrix(self, img_array, last_conv_layer_name="Conv_1", pred_index=None):
+        # create model to output the last convolutional layer
         convLayer = self.tf_model.get_layer(last_conv_layer_name).output
         grad_model = tf.keras.models.Model(
             [self.tf_model.inputs], [convLayer, self.tf_model.output]
         )
 
+        # get the gradients of the last convolutional layer
         with tf.GradientTape() as tape:
+            # forward pass
             last_conv_layer_logits, prediction_logits = grad_model(img_array)
             pred_index = tf.argmax(prediction_logits[0])
             loss_value = prediction_logits[:, pred_index]
 
+            # get the gradients of the last convolutional layer
             grads = tape.gradient(loss_value, last_conv_layer_logits)
             pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
 
+            # calculate Grad-CAM
             heatmap = tf.matmul(last_conv_layer_logits[0], pooled_grads[..., tf.newaxis])
             heatmap = tf.squeeze(heatmap)
             heatmap = tf.maximum(heatmap, 0) / tf.reduce_max(heatmap)
 
             return heatmap.numpy()
 
-    def create_heatmap(self, heatmap, image_size):
-        # Use RGB values of the colormap
+    def create_heatmap_from_gradcam(self, heatmap, image_size):
+        # use RGB values of the colormap
         nipy_cm = plt.cm.get_cmap("nipy_spectral")
         nipy_colors = nipy_cm(np.arange(256))[:, :3]
         heatmap_cm = nipy_colors[heatmap] * 255
 
-        # Create an image with RGB colorized heatmap
+        # create an image with RGB colorized heatmap
         return resize(heatmap_cm, image_size, anti_aliasing=True)
 
-    def create_superimposed_heatmap(self, original_img, heatmap, alpha=0.4):
+    def superimpose(self, original_img, heatmap, alpha=0.4):
+        # apply heatmap on top of original image
         return heatmap * alpha + original_img
 
-    def create_superimposed_mask(self, original_img, heatmap):
-        # Median/Mean thresholding
+    def mask_superimpose(self, original_img, heatmap):
+        # median thresholding
         threshold_level = np.median(heatmap)
         heatmap_mask = heatmap[:, :, 0] < threshold_level
 
+        # convert all pixels to black except for the heatmap
         heatmap_mask = heatmap_mask.astype(int)
         heatmap_mask[heatmap_mask == 1] = 255.0
 
+        # use bitwise or to mask original image
         base_img = original_img.astype(int)
         return np.bitwise_or(heatmap_mask[:, :, np.newaxis], base_img)
 
-    def internal_predict(self, image_path: str):
+    def classify_and_gradcam(self, image_path: str):
         # load image
         image_tensor = load_img(image_path, target_size=self.IMG_SIZE)
         image_tensor = img_to_array(image_tensor)
@@ -81,7 +115,7 @@ class PredictorModel:
         prediction = tf.argmax(prediction, axis=1)
 
         # run Grad-CAM algorithm
-        gradcam = self.gradcam_heatmap(image_tensor)
+        gradcam = self.create_gradcam_matrix(image_tensor)
         gradcam = np.uint8(255 * gradcam)
 
         return (prediction[0], gradcam)
@@ -90,8 +124,8 @@ class PredictorModel:
         if not self.initialized:
             raise Exception("Predictor model not initialized")
 
-        # Make prediction and get Grad-CAM
-        (predicted, heatmap_raw) = self.internal_predict(image_path)
+        # make prediction and get Grad-CAM
+        (predicted, gradcam) = self.classify_and_gradcam(image_path)
 
         # load original image
         original_img = load_img(image_path)
@@ -100,25 +134,25 @@ class PredictorModel:
 
         # create base heatmap using colormap
         heatmap_path = os.path.join(output_path, "heatmap.jpg")
-        heatmap_arr = self.create_heatmap(heatmap_raw, image_size)
+        heatmap_arr = self.create_heatmap_from_gradcam(gradcam, image_size)
         heatmap_img = array_to_img(heatmap_arr)
         heatmap_img.save(heatmap_path)
 
         # superimpose the heatmap on original image
-        heatmap_imposed_path = os.path.join(output_path, "superimposed.jpg")
-        heatmap_imposed_arr = self.create_superimposed_heatmap(original_img, heatmap_arr, alpha=1.0)
-        heatmap_imposed_img = array_to_img(heatmap_imposed_arr)
-        heatmap_imposed_img.save(heatmap_imposed_path)
+        superimposed_path = os.path.join(output_path, "superimposed.jpg")
+        superimposed_arr = self.superimpose(original_img, heatmap_arr, alpha=1.0)
+        superimposed_img = array_to_img(superimposed_arr)
+        superimposed_img.save(superimposed_path)
 
-        # Superimpose the heatmap on original image
+        # superimpose the heatmap on original image with mask
         masked_path = os.path.join(output_path, "masked.jpg")
-        masked_arr = self.create_superimposed_mask(original_img, heatmap_arr)
+        masked_arr = self.mask_superimpose(original_img, heatmap_arr)
         masked_img = array_to_img(masked_arr)
         masked_img.save(masked_path)
 
         return (
             self.class_names[predicted],
             heatmap_path,
-            heatmap_imposed_path,
+            superimposed_path,
             masked_path
         )
